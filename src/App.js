165f5b55a091f550917ab0400e86e4a16fdff351
Jennie Juno <jennie@noreply.com>
2024-11-12 20:51:08 +0900

    자바스크립트 업데이트

--- src/App.js
+++ src/App.js
@@ -3,28 +3,29 @@ import React, { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    // 클라이언트 사이드에서만 실행되도록 조건 추가
-    if (typeof window !== 'undefined' && window.Kakao) {
+    // Kakao 객체가 로드되었는지 확인
+    if (typeof window !== 'undefined' && !window.Kakao) {
       window.Kakao = window.Kakao || {};
+    }
 
-      window.kakaoAsyncInit = function () {
+    window.kakaoAsyncInit = function () {
+      if (window.Kakao && window.Kakao.Channel) {
         Kakao.Channel.createChatButton({
           container: '#kakao-talk-channel-chat-button',
         });
-      };
+      }
+    };
 
-      // 카카오 SDK 스크립트 로드
-      (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://t1.kakaocdn.net/kakao_js_sdk/2.7.3/kakao.channel.min.js';
-        js.integrity = 'sha384-bWRMLTxQRntA4wEpVvfY/jIkkrpCzdbFJETnRNTm9kIF82L5UJfEsuhlcaqNQWiO';
-        js.crossOrigin = 'anonymous';
-        fjs.parentNode.insertBefore(js, fjs);
-      })(document, 'script', 'kakao-js-sdk');
-    }
-  }, []); // 빈 배열을 두어 컴포넌트 마운트 시 한 번만 실행
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://t1.kakaocdn.net/kakao_js_sdk/2.7.3/kakao.channel.min.js';
+      js.integrity = 'sha384-bWRMLTxQRntA4wEpVvfY/jIkkrpCzdbFJETnRNTm9kIF82L5UJfEsuhlcaqNQWiO';
+      js.crossOrigin = 'anonymous';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'kakao-js-sdk');
+  }, []);
 
   return (
     <div
@@ -35,10 +36,9 @@ function App() {
       data-color="yellow"
       data-shape="pc"
       data-support-multiple-densities="true"
-  >
-  </div>
+    >
+    </div>
   );
 }
 
 export default App;
-
